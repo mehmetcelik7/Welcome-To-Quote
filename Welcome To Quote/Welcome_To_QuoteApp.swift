056b436c862e9a62583d9b02a17bf6a706703ceb
mehmet <mehmet.cel.ik@hotmail.com>
2025-03-25 09:07:46 +0300

    Initial Commit

---
+++ Welcome To Quote/Welcome_To_QuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Welcome_To_QuoteApp.swift
+//  Welcome To Quote
+//
+//  Created by mehmet Çelik on 25.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct Welcome_To_QuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
